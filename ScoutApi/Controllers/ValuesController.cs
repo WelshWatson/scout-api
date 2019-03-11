d5c07b31e37e00d520cc934347b5020f02416966
Owen Watson <owen_watson84@hotmail.com>
2019-03-11 22:04:32 +0000

    Add swagger config and basic mysql post endpoint

--- ScoutApi/Controllers/ValuesController.cs
+++ ScoutApi/Controllers/ValuesController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using MySql.Data.MySqlClient;
+using ScoutApi.Models;
+using System;
 using System.Web.Http;
 
 namespace ScoutApi.Controllers
@@ -10,31 +8,66 @@ namespace ScoutApi.Controllers
     [RoutePrefix("api/")]
     public class ValuesController : ApiController
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        public ValuesController()
         {
-            return new string[] { "value1", "value2" };
+            
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        [HttpPost]
+        public string SendGeoData([FromUri] tracking_locations data)
         {
-            return "value";
-        }
+            data.TeamID = 9999;
+            data.teamname = "Owens team";
+            data.latitude = "51.4771822";
+            data.longitude = "-3.1546003";
+            data.lasttracked = "1549376549";
+            data.staff = 0;
+            data.onsite = false;
+            data.score = null;
+            data.members = null;
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+            string connStr = "server=38fea24f2847fa7519001be390c98ae0acafe387;user=joem;database=tracking_locations;port=3306;password=wP5sdnRFPF9Cl6HT";
+            var conn = new MySqlConnection(connStr);
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+                string sql = 
+                    $"INSERT INTO " +
+                    $"  tracking_locations " +
+                    $"  (TeamID, teamname, latitude, longitude, lasttracked, staff, onsite, score, members)" +
+                    $"VALUES" +
+                    $" ({data.TeamID}, " +
+                    $"  {data.teamname}," +
+                    $"  {data.latitude}," +
+                    $"  {data.longitude}," +
+                    $"  {data.lasttracked}," +
+                    $"  {data.staff}, " +
+                    $"  {data.onsite}, " +
+                    $"  {data.score}," +
+                    $"  {data.members})";
+
+                MySqlCommand sqlCommand = new MySqlCommand(sql, conn);
+                MySqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine(sqlDataReader[0] + " -- " + sqlDataReader[1]);
+                }
+
+                sqlDataReader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            conn.Close();
+            Console.WriteLine("Done.");
+
+            return "";
         }
     }
 }
