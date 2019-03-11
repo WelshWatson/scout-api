ea94e5bb018e3eaf2bb9f70ffbbd785d6b7c82a6
Owen Watson <owen_watson84@hotmail.com>
2019-03-11 22:05:40 +0000

    add

---
+++ ScoutApi/Models/tracking_locations.cs
@@ -0,0 +1,15 @@
+﻿namespace ScoutApi.Models
+{
+    public class tracking_locations
+    {
+        public int TeamID { get; set; }
+        public string teamname { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string lasttracked { get; set; }
+        public int staff { get; set; }
+        public bool? onsite { get; set; }
+        public int? score { get; set; }
+        public int? members { get; set; }
+    }
+}
